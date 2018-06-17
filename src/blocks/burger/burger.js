4c078b8d05d9f3917a0e4650987c293c2e8ce8fa
Ruslan Namanov <ruslan.namanov@yandex.ru>
2018-06-17 19:08:45 +0300

    Update 2018-06-17T16:08:31.138Z

--- src/blocks/burger/burger.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.burger').on('click', function () {
-        $(this).toggleClass('burger--active');
-        $('.hero-menu').toggleClass('hero-menu--open');
-    });
-});
