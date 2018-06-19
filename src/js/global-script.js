ff71e6a92c2612223e23407a9445bdcb5b0bb262
Ruslan Namanov <ruslan.namanov@yandex.ru>
2018-06-19 10:45:58 +0300

    Add sidebar fixed for news

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -14,7 +14,7 @@ $( document ).ready(function() {
             //autoplay: true,
 
             responsive: {
-                375: {
+                345: {
                     edgePadding: 25,
                     gutter: 16
                 },
@@ -32,12 +32,36 @@ $( document ).ready(function() {
         });
     }
 
-    // var swiper = new Swiper('.swiper-container', {
-    //     slidesPerView: 1,
-    //     spaceBetween: 16,
-    //     freeMode: true,
-    // });
+  // Get the container element
+  var btnContainer = document.getElementById("filtersLabel");
+
+  // Get all buttons with class="btn" inside the container
+  var btns = btnContainer.getElementsByClassName("label--btn");
+
+  // Loop through the buttons and add the active class to the current/clicked button
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("label--active");
+      current[0].className = current[0].className.replace(" label--active", "");
+      this.className += " label--active";
+    });
+  }
 });
+
+//Липкий сайдбар в новостях
+var sidebar = new StickySidebar("#newsBar", {
+  containerSelector: "#newsContent",
+  innerWrapperSelector: ".news__bar-inner",
+  topSpacing: 20,
+  bottomSpacing: 40
+});
+
+// var sidebar = new StickySidebar('#sidebar', {
+//   containerSelector: '#main-content',
+//   innerWrapperSelector: '.sidebar__inner',
+//   topSpacing: 20,
+//   bottomSpacing: 20
+// });
 
 // Изоляция без jQuery
 // (function(){
